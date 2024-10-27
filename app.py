5e131a3e0ed030527373d993bc2b25453e65dfad
K_user <kenji.main.mail.002@gmail.com>
2024-10-27 11:21:16 +0900

    コメント

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from utils.data_ut
+ils import load_csv_data, get_columns
 from utils.plot_utils import create_plot
-import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('upload.html')  # アップロードフォーム用のHTMLテンプレート
-
-@app.route('/upload', methods=['POST'])
+# ファイルアップロードの処理
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    file = request.files['file']
-    if file:
-        graph_html = create_plot(file)
-        return render_template('plot.html', graph_html=graph_html)  # プロット表示用のHTMLテンプレート
-    return "ファイルがアップロードされていません。"
+    if request.method == 'POST':
+        file = request.files['file']
+        data = load_csv_data(file)
+        columns = get_columns(data)
+        return render_template('select.html', columns=columns)
+    return render_template('upload.html')
+
+# データ項目選択後の処理
+@app.route('/plot', methods=['POST'])
+def plot():
+    x_column = request.form['x_column']
+    y_column = request.form['y_column']
+    data = load_csv_data(request.files['file'])
+    img_path = create_plot(data, x_column, y_column)
+    return render_template('plot.html', img_path=img_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
 
 
